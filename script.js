d471affcf157c64c3ec590ba503abadd025cef57
SPellThhshi <bmtran102@student.hbuhsd.edu>
2024-12-04 00:50:55 +0000

    commit

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
-const COUNTER = document.getElementById("COUNTERf")
+const COUNTERf  = document.getElementById("COUNTERf");
 var cookieCount = 0;
-var Clickpersec = 0;
+var Clickpersec = 0; 
+var click = 0; 
+
 
 cookie.addEventListener("click", cookieClick);
-cookie.addEventListener("click", cookieClick);
-
 
 function cookieClick(){
     cookieCount += 1;
     counter.innerHTML = "counter: " + cookieCount;
+    click += 1;
 }
 
 function cps() {
-
-    cookieCount += cps;
-    lickpersec.innerHTML = "COUNTERf" + Clickpersec;
+    cookieCount += Clickpersec;
+    COUNTERf.innerHTML = "clicks/s: " + click;
+    counter.innerHTML = "counter: " + cookieCount;
+    click = 0;
 
     setTimeout(cps, 1000);
 }
 
 
-
+cps();
 
 
 
